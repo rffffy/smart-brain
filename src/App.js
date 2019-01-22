aa6745eb060be2ea74752b1fd2c410e10c4ebf27
rffffy <raafay.alam@gmail.com>
2019-01-22 01:16:09 +0100

    depolying to heroku

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ class App extends Component {
 
     onButtonSubmit = () => {
         this.setState({imageUrl: this.state.input});
-        fetch('http://localhost:3000/imageurl', {
+        fetch('https://glacial-thicket-54955.herokuapp.com/imageurl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -102,7 +102,7 @@ class App extends Component {
             .then(response => response.json())
             .then(response => {
                 if (response) {
-                    fetch('http://localhost:3000/image', {
+                    fetch('https://glacial-thicket-54955.herokuapp.com/image', {
                         method: 'put',
                         headers: {'Content-Type': 'application/json'},
                         body: JSON.stringify({
